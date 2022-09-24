1837f4cfbc6cf1f1cbc685bc782e3b96aefa197b
GYEONGWON GIM <gyeongwon7986@gmail.com>
2022-09-24 23:00:30 +0900

    initial commit

---
+++ mul_div_server.py
@@ -0,0 +1,58 @@
+# Import socket module
+import socket
+
+# Here we use localhost ip address
+# and port number
+LOCALHOST = socket.gethostbyname(socket.gethostname())
+serverPORT = 5050
+# calling server socket method
+server = socket.socket(socket.AF_INET,
+					socket.SOCK_STREAM)
+
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
+server.bind((LOCALHOST, serverPORT))
+server.listen(1)
+print("Server started")
+print("Waiting for client request..")
+# Here server socket is ready for
+# get input from the user
+clientConnection, clientAddress = server.accept()
+print("Connected client :", clientAddress)
+msg = ''
+# Running infinite loop
+while True:
+	data = clientConnection.recv(1024)
+	msg = data.decode()
+	if msg == 'Over':
+		print("Connection is Over")
+		break
+
+	print("Equation is recievied")
+	result = 0
+	operation_list = msg.split()
+	oprnd1 = operation_list[0]
+	operation = operation_list[1]
+	oprnd2 = operation_list[2]
+
+	# here we change str to int converstion
+	num1 = int(oprnd1)
+	num2 = int(oprnd2)
+	# Here we are perform basic arithmetic operation
+	if operation == "+":
+		result = num1 + num2
+	elif operation == "-":
+
+		result = num1 - num2
+	elif operation == "/":
+		result = num1 / num2
+	elif operation == "*":
+		result = num1 * num2
+
+	print("Send the result to client")
+	# Here we change int to string and
+	# after encode send the output to client
+	output = str(result)
+	clientConnection.send(output.encode())
+clientConnection.close()
